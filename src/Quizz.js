744dbddab03cc9c6014ba3b2487888e37a818944
Your Name <anupraj1854@gmail.com>
2022-10-24 21:49:04 +0530

    first commit

---
+++ src/Quizz.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Quizz = () => {
+  return (
+    <>
+      <div className="app">
+        <div className="question-section">
+          <h5>Score 5</h5>
+          <div className="question-count">
+            <span>Question 1 of 10</span>
+          </div>
+          <div className="question-text">aslkdflasfljas</div>
+        </div>
+        <div className="answer-section">
+          <div className="actions">
+            <button>PREV</button>
+            <button>NEXT</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Quizz;
